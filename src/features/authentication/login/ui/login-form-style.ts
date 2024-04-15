a36f1f1b79d2cad6436d3e3b680f5c1d1ae87a7d
mingming <jimin23623@gmail.com>
2024-04-15 22:56:45 +0900

    chore: form 스타일 파일 이동

---
+++ src/features/authentication/login/ui/login-form-style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const LoginFormWrap = styled.div`
+  max-width: 520px;
+  width: 100%;
+`;
--- src/features/authentication/login/ui/login-form.tsx
+++ src/features/authentication/login/ui/login-form.tsx
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 
 import { CommonInput } from '@shared/ui/inputs/common-input';
 
+import * as S from './login-form-style';
 import { usePostLogin } from '../hooks';
 
 export const LoginForm = () => {
@@ -24,44 +25,46 @@ export const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(handleSubmitLogin)}>
-      <CommonInput
-        label='이메일'
-        id='email'
-        inputType='email'
-        placeholder='이메일을 입력해 주세요.'
-        errorMessage={errors.email?.message?.toString()}
-        {...register('email', {
-          pattern: {
-            value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-            message: '올바른 이메일 주소가 아닙니다.',
-          },
-          required: {
-            value: true,
-            message: '이메일을 입력해 주세요.',
-          },
-        })}
-      />
-      <CommonInput
-        label='비밀번호'
-        id='password'
-        inputType='password'
-        placeholder='비밀번호를 입력해 주세요.'
-        errorMessage={errors.password?.message?.toString()}
-        {...register('password', {
-          pattern: {
-            value: /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/,
-            message: '비밀번호를 다시한번 확인해 주세요.',
-          },
-          required: {
-            value: true,
-            message: '비밀번호를 입력해 주세요.',
-          },
-        })}
-      />
-      <button type='submit' disabled={isLoading}>
-        {isLoading ? '잠시만 기다려 주세요...' : '로그인하기'}
-      </button>
-    </form>
+    <S.LoginFormWrap>
+      <form onSubmit={handleSubmit(handleSubmitLogin)}>
+        <CommonInput
+          label='이메일'
+          id='email'
+          inputType='email'
+          placeholder='이메일을 입력해 주세요.'
+          errorMessage={errors.email?.message?.toString()}
+          {...register('email', {
+            pattern: {
+              value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+              message: '올바른 이메일 주소가 아닙니다.',
+            },
+            required: {
+              value: true,
+              message: '이메일을 입력해 주세요.',
+            },
+          })}
+        />
+        <CommonInput
+          label='비밀번호'
+          id='password'
+          inputType='password'
+          placeholder='비밀번호를 입력해 주세요.'
+          errorMessage={errors.password?.message?.toString()}
+          {...register('password', {
+            pattern: {
+              value: /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/,
+              message: '비밀번호를 다시한번 확인해 주세요.',
+            },
+            required: {
+              value: true,
+              message: '비밀번호를 입력해 주세요.',
+            },
+          })}
+        />
+        <button type='submit' disabled={isLoading}>
+          {isLoading ? '잠시만 기다려 주세요...' : '로그인하기'}
+        </button>
+      </form>
+    </S.LoginFormWrap>
   );
 };
