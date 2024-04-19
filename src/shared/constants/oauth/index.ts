18878998507ccb121ec2f2ababd4857dfc58dc34
mingming <jimin23623@gmail.com>
2024-04-19 20:28:07 +0900

    chore: env 수정

--- src/shared/constants/oauth/index.ts
+++ src/shared/constants/oauth/index.ts
@@ -1,2 +1,2 @@
-export const API_NAVER_CLIENT_ID: string = process.env.API_CLIENT_ID || '';
-export const API_NAVER_CLIENT_SECRET: string = process.env.API_CLIENT_SECRET || '';
+export const API_NAVER_CLIENT_ID: string = process.env.NEXT_PUBLIC_API_CLIENT_ID || '';
+export const API_NAVER_CLIENT_SECRET: string = process.env.NEXT_PUBLIC_API_CLIENT_SECRET || '';
