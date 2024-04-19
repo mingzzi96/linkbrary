e746aad275fb6f43cc8e43a9532e42b69f813d4c
mingming <jimin23623@gmail.com>
2024-04-19 21:12:06 +0900

    chore: nextauth 다시 수정

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,17 @@
-import NextAuth from 'next-auth';
+import NextAuth from 'next-auth/next';
 import NaverProvider from 'next-auth/providers/naver';
 
 import { API_NAVER_CLIENT_ID, API_NAVER_CLIENT_SECRET } from '@shared/constants/oauth';
 
-const handler = NextAuth({
+const options = {
   providers: [
     NaverProvider({
       clientId: API_NAVER_CLIENT_ID,
       clientSecret: API_NAVER_CLIENT_SECRET,
     }),
+    // 다른 인증 프로바이더들을 여기에 추가할 수 있습니다.
   ],
-});
+  // 필요한 추가적인 옵션들을 설정할 수 있습니다.
+};
 
-export { handler as GET, handler as POST };
+export default NextAuth(options);
